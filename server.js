c00e181d9f6697172a764ea733b275d528c1149a
Anton Savoskin <anton@sliday.com>
2016-11-21 11:09:30 +0700

    Update middware for localhost

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect(`http://${req.hostname}${req.url}`);
+  } else {
+    next();
   }
 });
 app.use(express.static('public'));
 app.listen(port, () => {
-    console.log(`Express server is up on port ${port}`);
+  console.log(`Express server is up on port ${port}`);
 });
