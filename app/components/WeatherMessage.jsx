0ef27a8fdc9c5d994701d2cb70ef5a85db68f1e4
Anton Savoskin <anton@sliday.com>
2016-11-21 12:11:40 +0700

    Add styles to weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,8 +1,8 @@
 const React = require('react');
 
 const WeatherMessage = ({ location, temp }) => (
-  <h3>
-    It's it {temp} in {location}.
+  <h3 className="text-center">
+    It's it {temp}&#8451; in {location}.
   </h3>
 );
 
--- public/bundle.js
+++ public/bundle.js
@@ -26554,7 +26554,7 @@
 	      if (isLoading) {
 	        return React.createElement(
 	          'h3',
-	          null,
+	          { className: 'text-center' },
 	          'Fetching weather...'
 	        );
 	      } else if (temp && location) {
@@ -26566,8 +26566,8 @@
 	      'div',
 	      null,
 	      React.createElement(
-	        'h3',
-	        null,
+	        'h1',
+	        { className: 'text-center' },
 	        'Get Weather'
 	      ),
 	      React.createElement(WeatherForm, { onSearch: this.handleSearch }),
@@ -26602,7 +26602,7 @@
 	      React.createElement('input', { type: 'text', ref: 'location', placeholder: 'Enter city name' }),
 	      React.createElement(
 	        'button',
-	        null,
+	        { className: 'button expanded hollow' },
 	        'Get Weather'
 	      )
 	    );
@@ -26618,13 +26618,13 @@
 	const React = __webpack_require__(7);
 
 	const WeatherMessage = ({ location, temp }) => React.createElement(
-	  'h3',
-	  null,
-	  'It\'s it ',
+	  "h3",
+	  { className: "text-center" },
+	  "It's it ",
 	  temp,
-	  ' in ',
+	  "\u2103 in ",
 	  location,
-	  '.'
+	  "."
 	);
 
 	module.exports = WeatherMessage;
